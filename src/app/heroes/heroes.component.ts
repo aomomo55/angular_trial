76a8e9978a3676061f31d7ae52671e7d9a804b61
Momoko Aoki <sora.idk@gmail.com>
2020-05-05 09:52:29 +0900

    Observableデータのパートまで

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +8,25 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) {}
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm',
   }
 
-  heroes = HEROES
+  heroes: Hero[]
 
   selectedHero: Hero
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes))
+  }
+
+  ngOnInit() {
+    this.getHeroes()
+  }
 }
